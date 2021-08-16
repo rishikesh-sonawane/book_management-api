5a897e47ca436bdf747254e9b96ba213136935f4
Rishikesh_Sonawane <rishikeshsonawane1465@gmail.com>
2021-08-16 13:56:33 +0530

    temp database added

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
 
+// import database
+const Database = require("./database");
+
+
+// initialization
 const OurApp = express();
 
 OurApp.get("/", (request,response) => {
-    response.json({ message:"Request served!!!!!!!"});
+    response.json({ message:"Server is working well"});
+});
+
+OurApp.get("/book", (req, res) => {
+    return res.json({ books: Database.Book });
 });
 
 OurApp.listen(5001, () => console.log("Server is running"));
